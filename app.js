26abed8d38139afb6e61ec3142cf9ca6db1b8f7a
Hulk Hogan <atm1982@yahoo.com>
2022-12-21 11:07:10 -0500

    radio changes

--- app.js
+++ app.js
@@ -17,8 +17,35 @@ var myApp = angular.module("myApp", []);
 myApp.controller("myCtrl", function ($scope) {
     var c = this;
     c.testName = 'Mogambo';
-    $scope.firstName = "Atul";
-    $scope.lastName = "Mahajan";
+    $scope.firstName = "Michael";
+    $scope.lastName = "Jackson";
+
+    $scope.people = [{
+        name: "John",
+        id: "J"
+      }, {
+        name: "Paul",
+        id: "P"
+      }, {
+        name: "George",
+        id: "G"
+      }, {
+        name: "Ringo",
+        id: "R"
+      }];
+      $scope.myObj = {
+        person: $scope.people[1]
+      };
+
+      c.selectedContact = '';
+      c.phones = [{
+        type: 'businessPhone',
+        value: '+1-201-201-2011'
+      },{
+        type: 'mobilePhone',
+        value: '+1-202-202-2022'
+      }];
+
     c.priceMatrix = [{
         priceType: 'Manual',
         softwareGroup: 'Third Party',
@@ -33,6 +60,30 @@ myApp.controller("myCtrl", function ($scope) {
         verbiage: 'Manual, Third Party3'
     }];
 
+    c.priceMatrixObj = {
+        manual: {
+            priceType: 'Manual',
+            softwareGroup: 'Third Party',
+            verbiage: 'Manual, Third Party'
+        },
+        manual2: {
+            priceType: 'Manual2',
+            softwareGroup: 'Third Party2',
+            verbiage: 'Manual, Third Party2'
+        },
+        manual3: {
+            priceType: 'Manual3',
+            softwareGroup: 'Third Party3',
+            verbiage: 'Manual, Third Party3'
+        }
+    };
+
+    c.priceMatrixObj2 = {
+        manual: 'manual',
+        manual2: 'manual2',
+        manual3: 'manual3'
+    };
+    
     function checkVerbiage(v){
         return (v.priceType === 'Manual2' && v.softwareGroup === 'Third Party2');
     }
